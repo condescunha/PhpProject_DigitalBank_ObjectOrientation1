f905b9feeed30ceb43b9eb4190e7a234dc5b1f10
Marcondes Cunha Araujo <marcunharaujo@gmail.com>
2021-08-09 17:34:25 -0300

    Added deposit method.

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 require_once 'CPF.php';
 require_once 'Holder.php';
+require_once 'Account.php';
 
 /* 
  * Project Digital Bank for to study PHP language.
@@ -11,13 +12,21 @@ require_once 'Holder.php';
 //Test one ---------------------------------------
 $cpf = new CPF("246.648.246-00");
 $holder = new Holder("Marcondes Araújo", $cpf);
+$account = new Account($holder);
+
+$account->deposit(352.50);
 
 echo $holder->getName().PHP_EOL;
 echo $holder->getCpfNumber().PHP_EOL;
+echo $account->getBalance().PHP_EOL;
 
 //Test two ---------------------------------------
 $cpf2 = new CPF("467.635.759-45");
 $holder2 = new Holder("Luana Araújo", $cpf2);
+$account2 = new Account($holder2);
+
+$account2->deposit(215.36);
 
 echo $holder2->getName().PHP_EOL;
 echo $holder2->getCpfNumber().PHP_EOL;
+echo $account2->getBalance().PHP_EOL;
